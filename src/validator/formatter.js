0fec357813a4514102dc019d1a1e449db5b2987b
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-06-02 00:08:54 +0530

    space remove

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -9,14 +9,14 @@ module.exports.trim =trim
 
 
 const changeToLowerCase =function(){
-    const a = ("    Shubham Singh     ")
+    const a = ("Shubham Singh")
     let result =a.toLowerCase()
     console.log(result)
 }
 module.exports.changeToLowerCase=changeToLowerCase
 
 const changeToUpperCase=function(){
-    const a = ("    Shubham Singh     ")
+    const a = ("Shubham Singh")
     let result =a.toUpperCase()
     console.log(result)
 }
