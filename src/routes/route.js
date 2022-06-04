5f70a72c9aa9a15ccd1548a19e63d5a157e163a5
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-06-04 23:45:54 +0530

    missing number

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,63 +1,76 @@
 const express = require('express');
 const externalModule = require('./logger')
 
-const router = express.Router();
+const app = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
-});
+app.get('/sol1', function (req, res) {
+    let arr = [1, 2, 3, 5, 6, 7]
+    let item = arr[0]
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
+    for (let i = 0; i < arr.length + 1; i++)
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-
-
-    router.get('/sol1',function(req,res)
-{
-    let arr=[1,2,3,5,6,7]
-    let item=arr[0]
-    let n=0
-    for(let i=0;i<arr.length+1;i++)
-    
-        if(arr[i]==item){
+        if (arr[i] == item) {
             item++
-        }else{
-            missingNumber=item
+        } else {
+            missingNumber = item
         }
-    
+
     // const sumOfTheArray=(n*(n+1))/2-missingNumber
     console.log(missingNumber)
-    res.send({missingNumber});
+    res.send({ missingNumber });
 });
-router.get('/sol2',function(req,res)
-{
-    let arr=[33,34,35,37,38]
-    let item=arr[0]
-    let n=0
-    for(let i=0;i<arr.length+1;i++)
-    
-        if(arr[i]==item){
+app.get('/sol2', function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let item = arr[0]
+
+    for (let i = 0; i < arr.length + 1; i++)
+
+        if (arr[i] == item) {
             item++
-        }else{
-            missingNumber=item
+        } else {
+            missingNumber = item
         }
-    
+
     // const sumOfTheArray=(n*(n+1))/2-missingNumber  
     console.log(missingNumber)
-    res.send({missingNumber});
+    res.send({ missingNumber });
 });
 
-module.exports = router;
+app.get("/solv1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr = [1, 2, 3, 5, 6, 7]
+
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+
+    let lastDigit = arr.pop()
+    let consecutiveSum = lastDigit * (lastDigit + 1) / 2
+    let missingNumber = consecutiveSum - total
+
+    res.send({ data: missingNumber });
+});
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+app.get("/solv2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr = [33, 34, 35, 37, 38]
+    let len = arr.length
+
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+
+    let firstDigit = arr[0]
+    let lastDigit = arr.pop()
+    let consecutiveSum = (len + 1) * (firstDigit + lastDigit) / 2
+    let missingNumber = consecutiveSum - total
+
+    res.send({ data: missingNumber });
+});
+
+
+
+module.exports = app;
 // adding this comment for no reason
