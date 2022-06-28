b0724aed923b99c3f09803da64d28ca5a6e2abef
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-06-28 17:02:43 +0530

    add2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+const route = require('./src/routes/route.js');
 
 const app = express();
 
